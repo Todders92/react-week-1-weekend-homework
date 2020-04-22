13d80272ca9d2220b075b80ae88bdfdab1425974
Todd Walraven <cod4247@yahoo.com>
2020-04-22 00:29:39 -0700

    update to end of tuesday night homework

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,15 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React from "react";
+import Header from "./Header";
+import TicketControl from "./TicketControl";
 
-function App(){
-  const name = "Thato";
-  const name2 = "Haley";
+function App() {
   return (
-    <React.Fragment>
-      <h1>Help Queue</h1>
-      <h3>3a</h3>
-      <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      {/* This is a JSX comment. */}
-      <hr/>
-    </React.Fragment>
+    // <React.Fragment>
+    <div className="container">
+      <Header />
+      <TicketControl />
+    </div>
+    // </React.Fragment>
   );
 }
 
---
+++ src/components/TicketControl.js
@@ -0,0 +1,53 @@
+import React from "react";
+import NewTicketForm from "./NewTicketForm";
+import TicketList from "./TicketList";
+import PropTypes from "prop-types";
+
+class TicketControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+      masterTicketList: []
+    };
+  }
+
+  handleClick = () => {
+    this.setState((prevState) => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  };
+
+  handleAddingNewTicketToList = (newTicket) => {
+    const newMasterTicketList = this.state.masterTicketList.concat(newTicket);
+    this.setState({ masterTicketList: newMasterTicketList });
+    this.setState({ formVisibleOnPage: false });
+  };
+
+  render() {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />;
+      buttonText = "Return to Ticket List";
+    } else {
+      currentlyVisibleState = <TicketList />;
+      currentlyVisibleState = <TicketList ticketList={this.state.masterTicketList} />;
+      buttonText = "Add Ticket";
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    );
+  }
+}
+
+// Add propTypes for ticketList.
+TicketList.propTypes = {
+  ticketList: PropTypes.array
+};
+
+export default TicketControl;
